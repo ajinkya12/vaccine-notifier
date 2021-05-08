88aa4c856eb67b6c5bceb64c95e34f2298a11fff
Ajinkya D <dajinkya.12@gmail.com>
2021-05-08 22:24:02 +0530

    format response

--- app.js
+++ app.js
@@ -15,16 +15,46 @@ function available() {
 function sendMessage(centers) {
     var url = "ItyIUcdBtOp2Z2Q3JfrC0DqB/2NF1H14120B/F9NEV435T/secivres/moc.kcals.skooh//:sptth";
     var actual = url.split("").reverse().join("");
-    var text = "AVAILABLE VACCINE";
-    if(centers.length == 0) {
-        text = "NOT available";
-    }
+
+    var payload = formatResponse(centers);
     $.ajax({
         type: 'POST',
         url: actual,
-        data: 'payload=' + JSON.stringify({
-          "text": text
-        }),
+        data: 'payload=' + JSON.stringify(payload),
         dataType: "json"
     });
 }
+
+function formatResponse(centers) {
+    var res = {};
+    if(centers.length == 0) {
+        res['text'] = 'NOT AVAILABLE';
+        return res;
+    }
+    res['text'] = 'AVAILABLE VACCINE';
+    var blocks = [];
+    for(center in centers) {
+        var sessions = center['sessions'];
+        var min_age_limit = sessions[0]['min_age_limit'];
+        var available_capacity = sessions[0]['available_capacity'];
+        if(min_age_limit >= 18) {
+            var block = {};
+            block['type'] = 'section';
+            var text = {};
+            text['type'] = 'mrkdwn';
+            text['text'] = '*' + center['name'] + '* - *' + center['address'] + '* ';
+            block['text'] = text;
+
+            blocks.push(block);
+        }
+    }
+    return blocks;
+}
+/*
+{
+    "type": "section",
+    "text": {
+        "type": "mrkdwn",
+        "text": "*Danny Torrence* left the following review for your property:"
+    }
+*/
