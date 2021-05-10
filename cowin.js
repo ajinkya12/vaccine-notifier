71bf2b764869ad5b3e5a6d7a9b7c6401e1a85360
Ajinkya D <dajinkya.12@gmail.com>
2021-05-10 14:22:08 +0530

    http call

--- cowin.js
+++ cowin.js
@@ -3,6 +3,7 @@ const BASE_URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/c
 
 module.exports = {
     getCenters: (pincode, callback) => {
+        var headers = { ":authority" : "cdn-api.co-vin.in", ":method" : "GET", ":scheme" : "https", ":path" : "/api/v2/appointment/sessions/public/calendarByPin?pincode=444203&date=09-05-2021"}
         var nowDate = new Date();
         var date = nowDate.getDate() + '-' + (nowDate.getMonth() + 1) + '-' + nowDate.getFullYear();
         var params = { "pincode": pincode, "date": date };
@@ -10,10 +11,10 @@ module.exports = {
         console.log(date)
         var body = {}
         request.get({
-            url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin',
+            url: 'http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin',
             qs: params
         }, function (error, response, body) {
-            var responseObject = response.headers;
+            var responseObject = {};
             responseObject.body = body;
             callback(responseObject);
         })
